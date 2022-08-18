ad4e274be9d12d0c1f75df8daadfb0555447dc4d
lee <522460591@qq.com>
2022-08-18 15:21:09 +0800

    重构分离项目

---
+++ controller/UserController.go
@@ -0,0 +1,74 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"lee/ginessential/common"
+	"lee/ginessential/model"
+	"lee/ginessential/util"
+	"log"
+	"net/http"
+)
+
+func Register(ctx *gin.Context) {
+	DB := common.GetDB()
+
+	// 获取参数
+	name := ctx.PostForm("name")
+	telephone := ctx.PostForm("telephone")
+	password := ctx.PostForm("password")
+	// 数据验证
+	if len(telephone) != 11 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "手机号需要为11位",
+		})
+		return
+	}
+
+	if len(password) < 6 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "密码不能少于6位",
+		})
+		return
+	}
+
+	// 如果名称没用传，给一个10位随机字符串
+	if len(name) == 0 {
+		name = util.RandomString(10)
+	}
+
+	log.Println(name, telephone, password)
+	// 判断手机号是否存在
+	if isTelephoneExist(DB, telephone) {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{
+			"code": 422,
+			"msg":  "用户已经存在",
+		})
+		return
+	}
+
+	// 创建用户
+	newUser := model.User{
+		Name:      name,
+		Telephone: telephone,
+		Password:  password,
+	}
+	DB.Create(&newUser)
+
+	// 返回结果
+	ctx.JSON(200, gin.H{
+		"code": 200,
+		"msg":  "success",
+	})
+}
+
+func isTelephoneExist(db *gorm.DB, telephone string) bool {
+	var user model.User
+	db.Where("telephone = ?", telephone).First(&user)
+	if user.ID != 0 {
+		return true
+	}
+	return false
+}
