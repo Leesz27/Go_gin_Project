58b399d93b35bd767e72c55d5d0f85c379536656
lee <522460591@qq.com>
2022-08-18 23:13:16 +0800

    add config

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -3,12 +3,14 @@ package dto
 import "lee/ginessential/model"
 
 type UserDto struct {
+	Id        uint   `json:"UserId"`
 	Name      string `json:"name"`
 	Telephone string `json:"telephone"`
 }
 
 func ToUserDto(user model.User) UserDto {
 	return UserDto{
+		Id:        user.ID,
 		Name:      user.Name,
 		Telephone: user.Telephone,
 	}
